335aab0d1bc57b4ef4781b1f6b09bf52fdbe69c2
Sami Horttanainen <sami1horttanainen@gmail.com>
2023-01-16 09:58:30 +0200

    change test name

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-describe('Pokedex', function() {
+describe('Phonebook', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:3001')
     cy.contains('Phonebook')
